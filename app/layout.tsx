01c5e11874075081d8efc33c6cd7e0f4ceb51c7b
Godspower <gpsimi01@gmail.com>
2025-04-03 11:57:15 +0100

    PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,6 @@ import { Providers } from './providers'
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 const lexend = Lexend({
   subsets: ['latin'],
   weight: ['400', '500', '600', '800'],
@@ -25,6 +15,27 @@ const lexend = Lexend({
 export const metadata: Metadata = {
   title: "StrikeClimate. - Reducing hazard impact with technology...",
   description: "hazard, geospatial, data, processing, analyses, services, technology",
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "StrikeClimate",
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  icons: {
+    icon: [
+      { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [
+      { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -34,6 +45,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="theme-color" content="#000000" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body
         className={`${lexend.variable}`} data-qb-extension-installed="true" data-qb-installed="true"
       >
